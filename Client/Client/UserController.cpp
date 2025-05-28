50540bd02c627fc66d4248acbbdfa2b110518bb2
AmirHNzr <nazari.amirhossein@hotmail.com>
2025-05-28 15:19:42 +0330

    add basic network feature

---
+++ Client/Client/UserController.cpp
@@ -0,0 +1,67 @@
+#include "UserController.h"
+
+UserController::UserController(QObject *parent)
+    : QObject{parent}
+{
+
+    connect(&_socket,&QTcpSocket::connected,this,&UserController::connected);
+    connect(&_socket,&QTcpSocket::disconnected,this,&UserController::disconnected);
+    connect(&_socket,&QTcpSocket::stateChanged,this,&UserController::socket_stateChanged);
+    connect(&_socket,&QTcpSocket::errorOccurred,this,&UserController::errorOccurred);
+
+
+}
+
+void UserController::EstablishConnection(QString ip, int port)
+{
+    if(_socket.isOpen()){
+        if(this->ip == ip && this->port == port)
+            return;
+        _socket.close();
+    }
+    this->ip = ip;
+    this->port = port;
+    _socket.connectToHost(this->ip,this->port);
+
+}
+
+bool UserController::isConnected()
+{
+    return _socket.state() == QAbstractSocket::ConnectedState;
+}
+
+void UserController::disconnect()
+{
+    _socket.close();
+}
+
+QAbstractSocket::SocketState UserController::state()
+{
+    return _socket.state();
+}
+
+void UserController::sendJson(const QJsonObject &obj)
+{
+    auto bytes = QJsonDocument(obj).toJson(QJsonDocument::Compact) + "\n";
+    _socket.write(bytes);
+
+}
+
+void UserController::registerUser(const QString &username, const QString &password, const QString &email)
+{
+    QJsonObject req {
+        { "cmd",      "REGISTER"        },
+        { "username", username          },
+        { "password", password          },
+        { "email",    email             }
+    };
+    sendJson(req);
+}
+
+void UserController::socket_stateChanged(QAbstractSocket::SocketState state)
+{
+    if (state == QAbstractSocket::UnconnectedState) {
+        _socket.close();
+    }
+    emit stateChanged(state);
+}
---
+++ Client/Client/UserController.h
@@ -0,0 +1,43 @@
+#ifndef USERCONTROLLER_H
+#define USERCONTROLLER_H
+
+#include <QTcpSocket>
+#include <QObject>
+#include <QJsonDocument>
+#include <QJsonObject>
+class UserController : public QObject
+{
+    Q_OBJECT
+public:
+    explicit UserController(QObject *parent = nullptr);
+    void EstablishConnection(QString,int);
+    bool isConnected();
+
+
+    void disconnect();
+    QAbstractSocket::SocketState state();
+
+    // For sending information over network from client to server
+    void sendJson(const QJsonObject& obj);
+public slots:
+    // Registeration slot to send data to server
+    void registerUser(const QString& username,
+                      const QString& password,
+                      const QString& email);
+signals:
+    void connected();
+    void disconnected();
+    void stateChanged(QAbstractSocket::SocketState);
+    void errorOccurred(QAbstractSocket::SocketError);
+
+    private slots:
+    void socket_stateChanged(QAbstractSocket::SocketState state);
+
+
+private:
+    QTcpSocket _socket;
+    QString ip;
+    int port;
+};
+
+#endif // USERCONTROLLER_H
