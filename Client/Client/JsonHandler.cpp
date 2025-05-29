f5a8511b4bb830255c43eb4f49d99eea5ae7fc95
AmirHNzr <nazari.amirhossein@hotmail.com>
2025-05-29 22:13:13 +0330

    register checks done

--- Client/Client/JsonHandler.cpp
+++ Client/Client/JsonHandler.cpp
@@ -10,4 +10,10 @@ void JsonHandler::JsonReceived(QJsonDocument doc)
     auto cmd = obj["cmd"];
     if(cmd == "LOGIN_FAILED")
         QMessageBox::warning(nullptr,"Login error",obj["error"].toString());
+    else if(cmd == "REGISTER_FAILED")
+        QMessageBox::warning(nullptr,"Register error",obj["error"].toString());
+    else if(cmd == "REGISTER_SUCCESS")
+        QMessageBox::warning(nullptr,"Register Complete",obj["error"].toString());
+
+
 }
