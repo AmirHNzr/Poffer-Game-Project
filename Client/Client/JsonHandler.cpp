e0ad415c79961eef61a90340a84a662e4b1182f6
AmirHNzr <nazari.amirhossein@hotmail.com>
2025-05-31 14:58:14 +0330

    LOGIN_SUCCESS received, going to implement ui elements

--- Client/Client/JsonHandler.cpp
+++ Client/Client/JsonHandler.cpp
@@ -10,10 +10,12 @@ void JsonHandler::JsonReceived(QJsonDocument doc)
     auto cmd = obj["cmd"];
     if(cmd == "LOGIN_FAILED")
         QMessageBox::warning(nullptr,"Login error",obj["error"].toString());
+    else if(cmd == "LOGIN_SUCCESS")
+        QMessageBox::information(nullptr,"Login Complete",obj["error"].toString());
     else if(cmd == "REGISTER_FAILED")
         QMessageBox::warning(nullptr,"Register error",obj["error"].toString());
     else if(cmd == "REGISTER_SUCCESS")
-        QMessageBox::warning(nullptr,"Register Complete",obj["error"].toString());
+        QMessageBox::information(nullptr,"Register Complete",obj["error"].toString());
 
 
 }
